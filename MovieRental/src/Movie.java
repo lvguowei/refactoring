bc2c32bd1dd9642235a46abded57269d991b02e5
lvguowei <lvguowei1002@gmail.com>
2016-07-03 15:27:02 +0300

    use polymorphysim to replace case statements

--- MovieRental/src/Movie.java
+++ MovieRental/src/Movie.java
@@ -1,23 +1,35 @@
 
 public class Movie {
-    public static final int CHILDRENS = 2;
+    public static final int CHILDREN = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
-    public void setPriceCode(int code) {
-        _priceCode = code;
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDREN:
+                _price = new ChildrenPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
@@ -25,32 +37,10 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        // add bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        }
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
---
+++ MovieRental/src/Price.java
@@ -0,0 +1,11 @@
+
+public abstract class Price {
+
+    abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
+}
